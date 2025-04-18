bd5cf6cd61801405e28e273862c3269edfc5b4f5
Henrique-Camargo <henriquecnobrega0711@gmail.com>
2025-04-17 23:12:28 -0300

    Order, instant, ISO 8601 part 2

---
+++ src/main/java/dev/rick/course/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package dev.rick.course.repository;
+
+import dev.rick.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
