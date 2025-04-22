5c864fe42c4a5dcca8a987811ae6668d4d3c8e3a
Henrique-Camargo <henriquecnobrega0711@gmail.com>
2025-04-21 21:06:52 -0300

    Products

---
+++ src/main/java/dev/rick/course/entities/Product.java
@@ -0,0 +1,4 @@
+package dev.rick.course.entities;
+
+public class Product {
+}
---
+++ src/main/java/dev/rick/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package dev.rick.course.services;
+
+import dev.rick.course.entities.Order;
+import dev.rick.course.entities.User;
+import dev.rick.course.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
